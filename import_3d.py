605cb6876f7905e7086b439eb580164db5bf595a
Cris272500 <cristopherquincer@gmail.com>
2024-05-30 10:44:08 -0600

    ✅se agrega modelo 3d

--- import_3d.py
+++ import_3d.py
@@ -46,16 +46,6 @@ def main():
                     pygame.quit()
                     quit()
 
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        glTranslatef(-0.5,0,0)
-                    if event.key == pygame.K_RIGHT:
-                        glTranslatef(0.5,0,0)
-                    if event.key == pygame.K_UP:
-                        glTranslatef(0,1,0)
-                    if event.key == pygame.K_DOWN:
-                        glTranslatef(0,-1,0)
-
             glRotatef(1, 5, 1, 1)
             glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
